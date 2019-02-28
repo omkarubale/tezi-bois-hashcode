17118da1bec67bc9095e97df2db874e521f4a54f
omkarubale <oubale@gmail.com>
2019-02-28 23:51:54 +0530

    added requirements for the getImages.cpp file.

---
+++ getImages.cpp
@@ -0,0 +1,14 @@
+// struct image {
+//     int ImageNo,
+//     bool OrientationHorizontal,
+//     vector<string> ImageAttributes,
+// }
+
+//Output format : 
+//vector<struct image> imagesList;
+//Should contain all the images in the struct format
+
+//Also create : 
+//unordered_map<string, bool> imageAttributes
+//Stores which attributes have been given so that when we are creating the imageList
+//We have the attributes which are given to set to true.
