841dd8f1f65fa0b495ed6eb5bfcd7c3c8791217d
omkarubale <oubale@gmail.com>
2019-03-01 00:27:50 +0530

    Created structure for the program.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <math.h>
+#include <string>
+#include<vector>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Image {
+    int ImageNo;
+    bool OrientationHorizontal;
+    vector<string> ImageAttributes;
+};
+
+class Solution
+{
+    vector<struct Image> Images;
+
+    void getImages(string input)
+    {
+        //Get all images.
+    }
+};
 
 int main()
 {
     std::cout << "Hello World" << "\n";
+    return 0;
 }
